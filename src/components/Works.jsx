9af3cd1fdfafe2a6132712deaac872f1cba13244
astrospkc <punamkumari399@gmail.com>
2023-03-22 15:19:02 +0530

    project added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import weather from "./weather.png";
 import web from "./web.jpg";
 import movieFrontImage from "./movieFrontImage.png";
+import image from "./blogImage.png";
 
 const Works = () => {
   return (
@@ -14,7 +15,7 @@ const Works = () => {
         <div className="flex  flex-row m-4 border-2 border-black p-3 shadow-lg shadow-black">
           <img src={movieFrontImage} alt="" className="w-1/2" />
           <div className="mx-4">
-            <p className="">
+            <p className="multiline">
               This is a movie app where I used external tmdb APi and used tools
               such as reactjs , html and tailwind css.
             </p>
@@ -28,8 +29,22 @@ const Works = () => {
         <div className="flex  flex-row-reverse m-4 border-2 border-black p-3 shadow-lg shadow-black">
           <img src={weather} alt="" className="w-1/2" />
           <div className="mx-4">
-            <p className="">Tools used - Css , html and Javascript</p>
+            <p className="multiline">Tools used - Css , html and Javascript</p>
             <Link to="https://weather-info-eight.vercel.app/">
+              <button className="bg-blue-500 px-2 rounded-lg mt-4 ">
+                click here
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex  flex-row m-4 border-2 border-black p-3 shadow-lg shadow-black">
+          <img src={image} alt="" className="w-1/2" />
+          <div className="mx-4">
+            <p className="multiline">
+              Simple blogging website where we can write up our blog and post
+              it. Features added - delete , add, edit <div className=""></div>
+            </p>
+            <Link to="https://v-blog-7m8n.vercel.app/">
               <button className="bg-blue-500 px-2 rounded-lg mt-4 ">
                 click here
               </button>
